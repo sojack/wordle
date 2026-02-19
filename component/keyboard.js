eca138243d975e7234442e07ee0eee80c861839a
Jack So <sojack@gmail.com>
2026-02-18 20:19:10 -0500

    Support multiple guess rows with enter and delete handlers

--- component/keyboard.js
+++ component/keyboard.js
@@ -1,22 +1,5 @@
 
-export default function Keyboard({ setKeyPressed, setGuessPosition, setGuess, guessPosition }){
-    function deleteHandler() {
-        setGuessPosition( (prev) => {
-            if (prev>0){
-                return (prev-1)%6
-            }else{
-                return 0
-            }
-        } )
-
-        setGuess((prev)=>{
-            const x=prev.split("")
-            x[guessPosition]="_"
-            x[guessPosition-1]="_"
-            return x.join("")
-        })
-    }
-
+export default function Keyboard({ setKeyPressed, onDelete, onEnter }){
     function keyPressHandler(key){
         setKeyPressed(key)
     }
@@ -24,38 +7,38 @@ export default function Keyboard({ setKeyPressed, setGuessPosition, setGuess, gu
         <>
         <div className='keyboard'>
             <div className='keyboard_row'>
-                <button type="button" className='key' tabIndex="0" onMouseDown={()=>keyPressHandler("Q")}>Q</button>
-                <button type="button" className='key' tabIndex="1" onMouseDown={()=>keyPressHandler("W")}>W</button>
-                <button type="button" className='key' tabIndex="2" onMouseDown={()=>keyPressHandler("E")}>E</button>
-                <button type="button" className='key' tabIndex="3" onMouseDown={()=>keyPressHandler("R")}>R</button>
-                <button type="button" className='key' tabIndex="4" onMouseDown={()=>keyPressHandler("T")}>T</button>
-                <button type="button" className='key' tabIndex="5" onMouseDown={()=>keyPressHandler("Y")}>Y</button>
-                <button type="button" className='key' tabIndex="6" onMouseDown={()=>keyPressHandler("U")}>U</button>
-                <button type="button" className='key' tabIndex="7" onMouseDown={()=>keyPressHandler("I")}>I</button>
-                <button type="button" className='key' tabIndex="8" onMouseDown={()=>keyPressHandler("O")}>O</button>
-                <button type="button" className='key' tabIndex="9" onMouseDown={()=>keyPressHandler("P")}>P</button>
+                <button type="button" className='key' onMouseDown={()=>keyPressHandler("Q")}>Q</button>
+                <button type="button" className='key' onMouseDown={()=>keyPressHandler("W")}>W</button>
+                <button type="button" className='key' onMouseDown={()=>keyPressHandler("E")}>E</button>
+                <button type="button" className='key' onMouseDown={()=>keyPressHandler("R")}>R</button>
+                <button type="button" className='key' onMouseDown={()=>keyPressHandler("T")}>T</button>
+                <button type="button" className='key' onMouseDown={()=>keyPressHandler("Y")}>Y</button>
+                <button type="button" className='key' onMouseDown={()=>keyPressHandler("U")}>U</button>
+                <button type="button" className='key' onMouseDown={()=>keyPressHandler("I")}>I</button>
+                <button type="button" className='key' onMouseDown={()=>keyPressHandler("O")}>O</button>
+                <button type="button" className='key' onMouseDown={()=>keyPressHandler("P")}>P</button>
             </div>
             <div className='keyboard_row'>
-                <button type="button" className='key' tabIndex="10" onMouseDown={()=>keyPressHandler("A")}>A</button>
-                <button type="button" className='key' tabIndex="11" onMouseDown={()=>keyPressHandler("S")}>S</button>
-                <button type="button" className='key' tabIndex="12" onMouseDown={()=>keyPressHandler("D")}>D</button>
-                <button type="button" className='key' tabIndex="13" onMouseDown={()=>keyPressHandler("F")}>F</button>
-                <button type="button" className='key' tabIndex="14" onMouseDown={()=>keyPressHandler("G")}>G</button>
-                <button type="button" className='key' tabIndex="15" onMouseDown={()=>keyPressHandler("H")}>H</button>
-                <button type="button" className='key' tabIndex="16" onMouseDown={()=>keyPressHandler("J")}>J</button>
-                <button type="button" className='key' tabIndex="17" onMouseDown={()=>keyPressHandler("K")}>K</button>
-                <button type="button" className='key' tabIndex="18" onMouseDown={()=>keyPressHandler("L")}>L</button>
+                <button type="button" className='key' onMouseDown={()=>keyPressHandler("A")}>A</button>
+                <button type="button" className='key' onMouseDown={()=>keyPressHandler("S")}>S</button>
+                <button type="button" className='key' onMouseDown={()=>keyPressHandler("D")}>D</button>
+                <button type="button" className='key' onMouseDown={()=>keyPressHandler("F")}>F</button>
+                <button type="button" className='key' onMouseDown={()=>keyPressHandler("G")}>G</button>
+                <button type="button" className='key' onMouseDown={()=>keyPressHandler("H")}>H</button>
+                <button type="button" className='key' onMouseDown={()=>keyPressHandler("J")}>J</button>
+                <button type="button" className='key' onMouseDown={()=>keyPressHandler("K")}>K</button>
+                <button type="button" className='key' onMouseDown={()=>keyPressHandler("L")}>L</button>
             </div>
             <div className='keyboard_row'>
-                <button type="button" className='key long'>ENTER</button>
-                <button type="button" className='key' tabIndex="19" onMouseDown={()=>keyPressHandler("Z")}>Z</button>
-                <button type="button" className='key' tabIndex="20" onMouseDown={()=>keyPressHandler("X")}>X</button>
-                <button type="button" className='key' tabIndex="21" onMouseDown={()=>keyPressHandler("C")}>C</button>
-                <button type="button" className='key' tabIndex="22" onMouseDown={()=>keyPressHandler("V")}>V</button>
-                <button type="button" className='key' tabIndex="23" onMouseDown={()=>keyPressHandler("B")}>B</button>
-                <button type="button" className='key' tabIndex="24" onMouseDown={()=>keyPressHandler("N")}>N</button>
-                <button type="button" className='key' tabIndex="25" onMouseDown={()=>keyPressHandler("M")}>M</button>
-                <button type="button" className='key long' tabIndex="26" onMouseDown={deleteHandler}>Delete</button>
+                <button type="button" className='key long' onMouseDown={onEnter}>ENTER</button>
+                <button type="button" className='key' onMouseDown={()=>keyPressHandler("Z")}>Z</button>
+                <button type="button" className='key' onMouseDown={()=>keyPressHandler("X")}>X</button>
+                <button type="button" className='key' onMouseDown={()=>keyPressHandler("C")}>C</button>
+                <button type="button" className='key' onMouseDown={()=>keyPressHandler("V")}>V</button>
+                <button type="button" className='key' onMouseDown={()=>keyPressHandler("B")}>B</button>
+                <button type="button" className='key' onMouseDown={()=>keyPressHandler("N")}>N</button>
+                <button type="button" className='key' onMouseDown={()=>keyPressHandler("M")}>M</button>
+                <button type="button" className='key long' onMouseDown={onDelete}>Delete</button>
             </div>
         </div>
 
--- component/wordle.js
+++ component/wordle.js
@@ -4,6 +4,8 @@ import Keyboard from './keyboard'
 import Guess from './guess'
 import { TARGET_WORDS } from '../data/words'
 
+const MAX_GUESSES = 6
+
 function getTodaysWord() {
     const start = new Date(2024, 0, 1)
     const today = new Date()
@@ -13,19 +15,24 @@ function getTodaysWord() {
 
 export default function Wordle(){
     const [targetWord] = useState(getTodaysWord)
-    const [guess, setGuess] = useState('______')
+    const [guesses, setGuesses] = useState(Array(MAX_GUESSES).fill('______'))
+    const [currentRow, setCurrentRow] = useState(0)
     const [guessPosition, setGuessPosition] = useState(0)
     const [keyPressed, setKeyPressed] = useState()
     const notInitialRender = useRef(false)
-    
+
     useEffect(
         ()=>{
             if (notInitialRender.current) {
-                const x = guess.split('')
-                x[guessPosition]=keyPressed
-                setGuess(x.join(''))
+                setGuesses(prev => {
+                    const updated = [...prev]
+                    const x = updated[currentRow].split('')
+                    x[guessPosition]=keyPressed
+                    updated[currentRow] = x.join('')
+                    return updated
+                })
                 if (guessPosition<5){
-                    setGuessPosition( (guessPosition+=1)%6)
+                    setGuessPosition(guessPosition + 1)
                 }
 
             } else {
@@ -34,19 +41,53 @@ export default function Wordle(){
         },[keyPressed]
     )
 
+    function handleDelete() {
+        setGuessPosition(prev => {
+            if (prev > 0) return prev - 1
+            return 0
+        })
+        setGuesses(prev => {
+            const updated = [...prev]
+            const x = updated[currentRow].split('')
+            x[guessPosition] = '_'
+            if (guessPosition > 0) x[guessPosition - 1] = '_'
+            updated[currentRow] = x.join('')
+            return updated
+        })
+    }
+
+    function handleEnter() {
+        const current = guesses[currentRow]
+        if (current.includes('_')) return
+        if (currentRow < MAX_GUESSES - 1) {
+            setCurrentRow(currentRow + 1)
+            setGuessPosition(0)
+        }
+    }
+
     return(
     <>
-            {/* <p>Key pressed: {keyPressed}</p>
-            <p>Guess: {guess}</p>
-            <p>Guess Position: {guessPosition}</p> */}
+        <div className="board">
+            {guesses.map((g, i) => (
+                <Guess
+                    key={i}
+                    guess={g}
+                    guessPosition={i === currentRow ? guessPosition : -1}
+                />
+            ))}
+        </div>
 
-        <Guess guess={guess} guessPosition={guessPosition}/>
-
-        <Keyboard setKeyPressed={setKeyPressed} setGuessPosition={setGuessPosition} setGuess={setGuess} guessPosition={guessPosition} className='keyboard'/>
+        <Keyboard
+            setKeyPressed={setKeyPressed}
+            onDelete={handleDelete}
+            onEnter={handleEnter}
+        />
 
         <style jsx>{`
-            .keyboard{
-                margin:1em 0;
+            .board{
+                display:flex;
+                flex-direction:column;
+                align-items:center;
             }
         `}</style>
 
